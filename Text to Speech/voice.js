ee0fd5865829150e355cd9d8bfa25e5f936a2491
Harini-G12 <harinigowthaman5@gmail.com>
2024-11-15 11:04:54 +0530

    Add files via upload

---
+++ Text to Speech/voice.js
@@ -0,0 +1,10 @@
+var speech = new SpeechSynthesisUtterance();
+
+function speak(){
+    speech.text=textinput.value;
+
+    speech.rate=1;
+    speech.volume=1;
+    speech.pitch=1;
+    window.speechSynthesis.speak(speech);
+}
